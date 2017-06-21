f510822d16bcf6344440c06a81bfb4760a2abff3
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-21 16:34:10 +0100

    Ex9 - Appropriate plural form

--- directory.rb
+++ directory.rb
@@ -1,19 +1,22 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+
   students = []
-  # get the first name
+
   name = gets.chomp
-  # while the name is not empty, repeat this code
+
   while !name.empty? do
-    # add the student hash to the array
+
     students << {name: name, cohort: :november}
+    if students.count <= 1
+    puts "Now we have #{students.count} student"
+  else
     puts "Now we have #{students.count} students"
-    # get another name from user
+  end
     name = gets.chomp
   end
-  # return the array of students
+
   students
   end
 
@@ -28,8 +31,13 @@ def print(students)
   end
 end
 
+
 def print_footer(students)
+  if students.count <= 1
+  puts "Overall, we have #{students.count} great student"
+else
   puts "Overall, we have #{students.count} great students"
+  end
 end
 
 students = input_students
