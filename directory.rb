d5c7f1c3a5a19230f66db85c2f3edae6d5a94e6e
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-21 14:29:35 +0100

    Ex1 - modified each() method to print number before each students name

--- directory.rb
+++ directory.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.to_enum.with_index(1).each do |student, i|
+    puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
