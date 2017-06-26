40b06756af1b9acace6b81824a55757bec5e3c49
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-26 21:01:37 +0100

    More refatcoring

--- directory.rb
+++ directory.rb
@@ -1,9 +1,8 @@
+@students = [] #an empty array accessible to all methods
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
-  students = []
 
   name = gets.chomp
 
@@ -20,12 +19,12 @@ def input_students
 
     cohort =  gets.chomp
 
-    students << {name: name, age: age, location: location, cohort: cohort}
+    @students << {name: name, age: age, location: location, cohort: cohort}
 
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
   else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
 
     puts "Please add the next student"
@@ -33,11 +32,48 @@ def input_students
 
   end
 # Added if statement below to exit the code if no students are entered.
-  if students.length == 0
+  if @students.length == 0
     puts "NOTICE: You have not entered any students, please try again."
     exit
   end
-    students
+end
+
+=begin
+def print_students_list(students)
+  @students.map { |student| student[:cohort]}
+end
+=end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, please try again"
+  end
 end
 
 def print_header
@@ -45,9 +81,9 @@ def print_header
   puts "-------------".center(85)
 end
 
-def print_centered(students, cohort = "July")
+def print_students_list(cohort = "July")
   i = 0
-  students.each do |student|
+  @students.each do |student|
     if student[:cohort].empty?
       puts "#{student[:name]}, age: #{student[:age]}, from: #{student[:location]} (#{cohort.to_s} cohort)".center(85)
     else
@@ -57,52 +93,12 @@ def print_centered(students, cohort = "July")
   end
 end
 
-=begin
-def print_students_list(students)
-  students.map { |student| student[:cohort]}
-end
-=end
-
-def print_footer(students)
-  if students.count == 1
-    puts puts "Overall, we have #{students.count} great student.".center(85)
+def print_footer
+  if @students.count == 1
+    puts puts "Overall, we have #{@students.count} great student.".center(85)
   else
-    puts "Overall, we have #{students.count} great students.".center(85)
-  end
-end
-
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      # input the students
-      students = input_students
-    when "2"
-      # show the students
-      print_header
-      print_centered(students)
-      print_footer(students)
-    when "9"
-      exit #this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    puts "Overall, we have #{@students.count} great students.".center(85)
   end
 end
 
 interactive_menu
-
-
-
-
-
-
-#print_students_list(students)
